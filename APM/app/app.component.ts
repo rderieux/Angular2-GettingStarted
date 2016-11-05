a0793023056a5977a6797977a86448f71fdcad4b
rderieux <ppldiecomplex@gmail.com>
2016-11-04 20:51:39 -0600

    added product-list.component.ts, added product.ts interface and changed show image button to show/hide image

--- APM/app/app.component.ts
+++ APM/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'pm-app',
-    templateUrl: 'app/products/product-list.component.html'
-    // template: `
-    // <div><h1>{{pageTitle}}</h1>
-    //     <div>My First Component</div>
-    // </div>
-    // `
+    templateUrl: 'app/products/product-list.component.html',
+    template: `
+    <div><h1>{{pageTitle}}</h1>
+        <pm-products></pm-products>
+    </div>
+    `
 })
 export class AppComponent {
     pageTitle: string = 'Acme Product Management';
